efd2bf9bb8f8e1d85cc2d981b7a9cbb0be4153d2
999Marv <marvinsiri123@gmail.com>
2024-08-20 11:05:59 -0400

    Sending email works

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/999Marv/Go-Emailer
 
 go 1.22.6
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/smtp"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func sendEmail() {
+	godotenv.Load()
+	auth := smtp.PlainAuth(
+		"",
+		os.Getenv("MYEMAIL"),
+		os.Getenv("PASSWORD"),
+		"smtp.gmail.com",
+	)
+
+	err := smtp.SendMail(
+		"smtp.gmail.com:587",
+		auth,
+		os.Getenv("MYEMAIL"),
+		[]string{os.Getenv("MYEMAIL")},
+		[]byte("Subject: HI\nThis is you"),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
 
 func main() {
-	fmt.Printf("helloo")
+	sendEmail()
 }
